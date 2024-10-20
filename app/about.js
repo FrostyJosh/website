54f6f069e7dd216a60b85ba4eb6e94d5afa801ee
FrostyJosh <112000882+FrostyJosh@users.noreply.github.com>
2024-10-20 00:28:44 -0700

    Fixed unescaped entities in about.js

--- app/about.js
+++ app/about.js
@@ -4,7 +4,7 @@ export default function About() {
         <h1 className="text-4xl font-bold mb-4">About Me</h1>
         <p className="text-lg mb-6">
           Hi! I&apos;m [Joshua-Jaye Chan], a Computer Science major passionate about game development!
-          I&apos;m a beginner in React, Next.js, and other web technologies but more proficient with game engines such as Unity. Check out my projects below
+          I&apos;m a beginner in React, Next.js, and other web technologies but more proficient with game engines such as Unity. Check out my projects below.
         </p>
       </section>
     );
