4e7b95450d88ab19c43e773533a75c77c36d7709
FrostyJosh <112000882+FrostyJosh@users.noreply.github.com>
2024-10-20 16:24:20 -0700

    Fixed buttons on slideshow

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -12,6 +12,12 @@ const CustomCarousel = () => {
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
   const goToNext = () => {
     const isLastSlide = currentIndex === images.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
@@ -35,9 +41,17 @@ const CustomCarousel = () => {
           layout="fill"
           objectFit="cover"  // Ensures the image covers the whole container
         />
+        <button className="carousel-button prev" onClick={goToPrevious}>
+            &#10094;
+        </button>
+        <button className="carousel-button next" onClick={goToNext}>
+            &#10095;
+        </button>
       </div>
     </div>
   );
 };
 
 export default CustomCarousel;
+
+
